584e1c75bb1a24d7fb0d70561affa8ef843b6b81
gbatistaa <gbatistadev@gmail.com>
2026-03-02 21:34:13 -0300

    refactor: add Lombok annotations to UserDTO.

--- src/main/java/com/gabriel/mylibrary/user/dtos/UserDTO.java
+++ src/main/java/com/gabriel/mylibrary/user/dtos/UserDTO.java
@@ -1,5 +1,12 @@
 package com.gabriel.mylibrary.user.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
   private String username;
   private String email;
