9afaad03b9abb7f42f0ad39a9f41848cead683b7
gbatistaa <gbatistadev@gmail.com>
2026-03-04 09:21:04 -0300

    refactor: replace explicit imports with wildcard imports in BookEntity.

--- src/main/java/com/gabriel/mylibrary/auth/SecurityConfig.java
+++ src/main/java/com/gabriel/mylibrary/auth/SecurityConfig.java
@@ -1,9 +1,11 @@
-package com.gabriel.mylibrary;
+package com.gabriel.mylibrary.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +19,10 @@ public class SecurityConfig {
             .anyRequest().permitAll())
         .csrf(csrf -> csrf.disable());
     return http.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
