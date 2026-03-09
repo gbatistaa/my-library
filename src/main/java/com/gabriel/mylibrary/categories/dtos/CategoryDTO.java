70b1e6720ba7ac7d84ca4985f216529422a58ede
gbatistaa <gbatistadev@gmail.com>
2026-03-08 23:00:31 -0300

    feat: add id field to CategoryDTO

--- src/main/java/com/gabriel/mylibrary/categories/dtos/CategoryDTO.java
+++ src/main/java/com/gabriel/mylibrary/categories/dtos/CategoryDTO.java
@@ -1,5 +1,7 @@
 package com.gabriel.mylibrary.categories.dtos;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
+  private UUID id;
   private String name;
   private String description;
 }
